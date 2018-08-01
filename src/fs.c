eb0ef008ec45e2b57093b4ff973ba891d0f400b8
Tim Lebel <tim.lebel@smartsheet.com>
2018-07-31 17:23:14 -0700

    POC writing to filesystem

--- src/fs.c
+++ src/fs.c
@@ -7,13 +7,30 @@
 
 #include "messages.h"
 
+static const char SRVX_MSG_TYPE_REQ[] = "/req";
+
+srvx_mq_client mqclient;
+
+// Tells us whether a given path matches our route key schema.
+// The root "/" is always valid
+static int
+srvx_is_valid_path(const char *path)
+{
+    if (strcmp("/", path) == 0) {
+        return 1;
+    } else if (strcmp(SRVX_MSG_TYPE_REQ, path) == 0) {
+        return 1;
+    }
+    return 0;
+}
+
 static int
 srvx_getattr(const char *path, struct stat *stbuf)
 {
     printf("getattr called %s\n", path);
     memset(stbuf, 0, sizeof(struct stat));
 
-    if (strcmp(path, "/") == 0) { /* The root directory of our file system. */
+    if (srvx_is_valid_path(path)) {
         stbuf->st_mode = S_IFDIR | 0755;
         stbuf->st_nlink = 2;
     } else {
@@ -56,12 +73,44 @@ srvx_getxattr(const char *path, const char *one, char *two, size_t size)
     return 0;
 }
 
-static int 
+static int
 srvx_write(const char *path, const char *data, size_t size, off_t offset,
 		struct fuse_file_info *info)
 {
     printf("Writing to path %s data %s\n", path, data);
+    char *msg = strdup(data);
+    srvx_mq_client_send(&mqclient, msg);
     return size;
+}
+
+static int
+srvx_open(const char *path, struct fuse_file_info *info)
+{
+    return 0;
+}
+
+static int
+srvx_create(const char *path, mode_t mode, struct fuse_file_info *info)
+{
+    return 0;
+}
+
+static int
+srvx_utimens(const char *path, const struct timespec tv[2])
+{
+    return 0;
+}
+
+static int
+srvx_truncate(const char *path, off_t offset)
+{
+    return 0;
+}
+
+static int
+srvx_chown(const char *path, uid_t uid, gid_t gid)
+{
+    return 0;
 }
 
 static struct fuse_operations srvx_filesystem_operations = {
@@ -71,19 +120,18 @@ static struct fuse_operations srvx_filesystem_operations = {
     .flush    = srvx_flush,
     .getxattr = srvx_getxattr,
     .write    = srvx_write,
+    .open     = srvx_open,
+    .create   = srvx_create,
+    .utimens  = srvx_utimens,
+    .truncate = srvx_truncate,
+    .chown    = srvx_chown,
 };
 
 int
 main(int argc, char **argv)
 {
     printf("Starting the program\n");
-    srvx_mq_client mqclient;
     srvx_mq_client_connect(&mqclient);
-
-    printf("Sending message to server\n");
-    srvx_mq_client_send(&mqclient, "howdy");
-
+    fuse_main(argc, argv, &srvx_filesystem_operations, NULL);
     srvx_mq_client_destroy(&mqclient);
-    // return fuse_main(argc, argv, &srvx_filesystem_operations, NULL);
-    return 0;
 }
